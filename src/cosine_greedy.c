77a96cb4dd91d4b19db031ff2796d12805a830fc
Marek Mosna <marek.mosna@gmail.com>
2022-09-11 23:27:12 +0200

    Fix cosine file headers

--- src/cosine_greedy.c
+++ src/cosine_greedy.c
@@ -1,3 +1,21 @@
+/* 
+ * This file is part of the PGMS distribution (https://github.com/genesissoftware-tech/pgms or https://ip-147-251-124-124.flt.cloud.muni.cz/chemdb/pgms).
+ * Copyright (c) 2022 Marek Mosna (marek.mosna@genesissoftware.eu).
+ * Copyright (c) 2022 Jakub Galgonek (jakub.galgonek@uochb.cas.cz) as initial contributor
+ *
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <float.h>
 #include <postgres.h>
 #include <fmgr.h>
